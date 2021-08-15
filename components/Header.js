b9e01c31d42668495e7b09cdf131f7947af1fea6
SushiJ <74211783+SushiJ@users.noreply.github.com>
2021-08-15 13:52:04 +0530

    💄 Header Done, need to look into UX a bit more

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center justify-between h-auto m-5 sm:flex-row">
+      <div className="flex flex-grow max-w-2xl justify-evenly">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <div>
+        <Image
+          src="https://links.papareact.com/ua6"
+          width={200}
+          height={100}
+          className="object-contain"
+          alt="Hulu Logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <div>
+      <h1>This is the nav baby</h1>
+    </div>
+  );
+}
+
+export default Nav;
