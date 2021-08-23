0c48c72a8465510e405b638d2ed4f65bf09ac081
SushiJ <74211783+SushiJ@users.noreply.github.com>
2021-08-24 00:45:23 +0530

    🚀 Completed Baby

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,23 @@
+import { useRouter } from "next/dist/client/router";
+import requests from "../utils/requests";
+
 function Nav() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>This is the nav baby</h1>
-    </div>
+    <nav className="relative">
+      <div className="flex px-10 space-x-10 overflow-x-scroll text-2xl whitespace-nowrap sm:px-20 sm:space-x-20 scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            onClick={() => router.push(`/?genre=${key}`)}
+            className="transition duration-100 transform cursor-pointer last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+    </nav>
   );
 }
 
